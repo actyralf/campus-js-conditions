6df07144a7187fee1eee133da0ae09013879f041
Ralf Siewert <ralf@actyvy.st>
2024-09-06 11:22:08 +0200

    exercise2

--- exercises.js
+++ exercises.js
@@ -21,7 +21,11 @@ if (number > 10) {
 // If it is even, print "The number is even".
 // If it is odd, print "The number is odd".
 
-// write your code here
+if (number % 2 === 0) {
+  console.log("The number is even");
+} else {
+  console.log("The number is odd");
+}
 
 // Exercise 3
 // Age Verification
